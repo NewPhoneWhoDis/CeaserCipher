1bd3f18436c8aee189b60d94881d3798f3c7d903
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2021-10-30 23:12:18 +0200

    Added GUI with Encrypt and Decrypt function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        CaesarCipher caesarCipher = new CaesarCipher();
-        Scanner userInput = new Scanner(System.in);
+        //CaesarCipher caesarCipher = new CaesarCipher();
+        //Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Enter a message without special symbols like @, &, etc.");
-        System.out.println("After that enter an offset number!");
+        //System.out.println("Enter a message without special symbols like @, &, etc.");
+        //System.out.println("After that enter an offset number!");
 
-        String result = caesarCipher.cipher(userInput.nextLine(), userInput.nextInt());
+        //String result = caesarCipher.cipher(userInput.nextLine(), userInput.nextInt());
 
-        System.out.println("The ciphered message is : " + result);
-        System.out.println("Enter an offset to decipher the message!");
-        System.out.println("Following message is deciphered: " + caesarCipher.decipher(result, userInput.nextInt()));
+        //System.out.println("The ciphered message is : " + result);
+        //System.out.println("Enter an offset to decipher the message!");
+        //System.out.println("Following message is deciphered: " + caesarCipher.decipher(result, userInput.nextInt()));
+
+        CaesarCipherGUI gui = new CaesarCipherGUI();
     }
 
 }
